c07b8a33f4958fbf58672aefa299166739d40947
VasilyevVD <kobe001@mail.ru>
2025-01-11 23:07:02 +0400

    1.7. Конструктор [#173889 #527837]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+
+    }
+
+    public void printInfo() {
+        System.out.println("Ошибка ? " + active);
+        System.out.println("Номер ошибки: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 57, "ошибка ввода");
+        error.printInfo();
+        Error notError = new Error();
+        notError.printInfo();
+    }
+}
