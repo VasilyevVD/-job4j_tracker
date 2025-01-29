99386ca4092754898891d36ed254641aad884174
VasilyevVD <kobe001@mail.ru>
2025-01-29 12:18:19 +0400

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,15 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created;
 
     public Item() {
-
+        this.created = LocalDateTime.now();
     }
 
     public Item(String name) {
@@ -31,5 +35,19 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public class StarUI {
+        public static void main(String[] args) {
+            Item item = new Item();
+            LocalDateTime created = item.getCreated();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+            String formattedDate = created.format(formatter);
+            System.out.println(formattedDate);
+        }
     }
 }
