43a35e76f3030e1332e8e6d510fa6a412e60c77d
VasilyevVD <kobe001@mail.ru>
2025-02-03 22:41:14 +0400

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,34 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book cleanCode = new Book("Clean Code", 0);
+        Book abc = new Book("ABC", 45);
+        Book pinocchio = new Book("Pinocchio", 56);
+        Book lionKing = new Book("Lion King", 67);
+        Book[] books = new Book[4];
+        books[0] = cleanCode;
+        books[1] = abc;
+        books[2] = pinocchio;
+        books[3] = lionKing;
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println(book.getName() + " - " + book.getPages());
+        }
+        System.out.println("Swapped 3 and 0 book");
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println(book.getName() + " - " + book.getPages());
+        }
+        System.out.println("Outputting books with the name Clean code");
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            if (book.equals(cleanCode)) {
+                System.out.println("Clean Code");
+            }
+        }
+    }
+}
