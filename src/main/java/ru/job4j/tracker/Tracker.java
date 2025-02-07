633d2f8a00c6b1a2faee64ba556ed9159110001a
VasilyevVD <kobe001@mail.ru>
2025-02-07 16:45:35 +0400

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,31 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll(){
+        Item[] result = new Item[size];
+        int resultSize = 0;
+        for (int index = 0; index <= resultSize; index++) {
+            Item item = items[index];
+            if (items[index] != null) {
+                result[resultSize] = item;
+                resultSize++;
+            }
+        }
+        return Arrays.copyOf(result, resultSize);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int resultSize = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if(key.equals(item.getName())) {
+                result[resultSize++] = item;
+            }
+        }
+        return Arrays.copyOf(result,resultSize);
     }
 
     public Item findById(int id) {
