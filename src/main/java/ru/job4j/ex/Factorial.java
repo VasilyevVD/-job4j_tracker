673e82281b131d9af18eb4c848072aae3bc139b5
VasilyevVD <kobe001@mail.ru>
2025-03-06 14:10:46 +0400

    0.3. Тестирование исключений с junit [#219366 #534946]

--- src/main/java/ru/job4j/ex/Factorial.java
+++ src/main/java/ru/job4j/ex/Factorial.java
@@ -4,6 +4,8 @@ public class Factorial {
     public int calc(int number) {
         if (number < 0) {
             throw new IllegalArgumentException("Number could not be less than 0");
+        } else {
+            System.out.println("Расчеты верны.");
         }
         int result = 1;
         for (int index = 2; index <= number; index++) {
