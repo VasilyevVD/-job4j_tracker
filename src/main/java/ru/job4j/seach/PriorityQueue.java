23342ec9d209593896cc9a28c4f0ae14365f64ed
VasilyevVD <kobe001@mail.ru>
2025-05-15 13:32:09 +0400

    5. Очередь с приоритетом на LinkedList [#41670 #543355]

--- src/main/java/ru/job4j/seach/PriorityQueue.java
+++ src/main/java/ru/job4j/seach/PriorityQueue.java
@@ -10,7 +10,6 @@ public class PriorityQueue {
         for (Task element : tasks) {
             if (element.getPriority() <= task.getPriority()) {
                 index++;
-            } else {
                 break;
             }
         }
