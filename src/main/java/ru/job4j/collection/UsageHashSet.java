725a7e24a0343a944ea705b28749e575966088b4
VasilyevVD <kobe001@mail.ru>
2025-06-03 22:20:17 +0400

    0. Set, HashSet, Iterator. [#10095 #545235]

--- src/main/java/ru/job4j/collection/UsageHashSet.java
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -8,8 +8,11 @@ public class UsageHashSet {
         autos.add("Lada");
         autos.add("BMW");
         autos.add("Volvo");
+        autos.add("Lada");
         autos.add("Toyota");
-        for (String auto:autos) {
+        autos.add("BMW");
+        autos.add("Volvo");
+        for (String auto : autos) {
             System.out.println(auto);
         }
     }
